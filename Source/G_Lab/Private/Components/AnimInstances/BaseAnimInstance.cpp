3fb0347f490cc64147d91d64a76d2e5a6bd3f3ab
pedro-queiroz <pedroxsqueiroz@gmail.com>
2024-09-05 22:54:38 -0300

    Initial commit

---
+++ Source/G_Lab/Private/Components/AnimInstances/BaseAnimInstance.cpp
@@ -0,0 +1,271 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/AnimInstances/BaseAnimInstance.h"
+
+#include "GameFramework/Character.h"
+
+#pragma optimize("", off)
+FIKData UBaseAnimInstance::GetIKData(const FIKParams& ikParams, bool& hitted)
+{
+    
+    /************
+    * GET WEIGHTS
+    *************/
+    bool getWeightByCurve = ikParams.WeightCurveName.IsValid()
+                        &&  !ikParams.WeightCurveName.IsNone()
+                        &&  ikParams.WeightCurveName.GetStringLength() > 0;
+    float currentWeight = getWeightByCurve ?
+            this->GetCurveValue(ikParams.WeightCurveName) 
+        :   ikParams.Weight;
+
+    UE_LOG(LogTemp, Log, TEXT("WEIGHT:%.2f"), currentWeight);
+    
+    bool getLockWeightByCurve = ikParams.LockWeightCurveName.IsValid() 
+                            &&  !ikParams.LockWeightCurveName.IsNone()
+                            &&  ikParams.LockWeightCurveName.GetStringLength() > 0;
+    float currentLockWeight = getLockWeightByCurve ?
+            this->GetCurveValue(ikParams.LockWeightCurveName)
+        :   ikParams.LockWeight;
+
+
+    /**********************
+    * CALCULATE START TRACE
+    ***********************/
+    FVector startTrace = FVector::Zero();
+    FVector startReference = FVector::Zero();
+    if (ikParams.StartTraceBoneReference.IsValid() && ikParams.StartTraceBoneReference.GetStringLength() > 0) 
+    {
+        startReference = this->GetOwningComponent()->GetSocketLocation(ikParams.StartTraceBoneReference) * ikParams.StartTraceMask;
+        startTrace = FVector(startReference);
+
+        if (ikParams.AddRelativeLocationFromReverseMask) 
+        {
+            FVector reverseMask = FVector(1) - ikParams.StartTraceMask;
+            startTrace += reverseMask * ikParams.ReverseMaskStartTraceLocation;
+        }
+    }
+    else 
+    {
+        startTrace = ikParams.StartTraceLocation;
+    }
+
+    /*****************
+    * FIND IK LOCATION
+    ******************/
+    FHitResult traceResult;
+    FCollisionQueryParams params;
+    
+    hitted = this->GetWorld()->SweepSingleByChannel(
+        traceResult,
+        startTrace,
+        startTrace + ( ikParams.TraceDirection * ikParams.TraceLength ),
+        FQuat::Identity,
+        ECollisionChannel::ECC_Visibility,
+        FCollisionShape::MakeSphere(ikParams.TraceRadius),
+        params
+    );
+
+    if (hitted) 
+    {
+        FVector ikLocation = FMath::Lerp( 
+                traceResult.ImpactPoint + ((ikParams.TraceDirection * -1) * ikParams.Padding)
+            ,   ikParams.CurrentLockLocation
+            ,   currentLockWeight
+        );
+
+        return FIKData(
+                startReference
+            ,   ikLocation
+            ,   currentWeight
+        );
+
+    }
+
+    return FIKData();
+}
+#pragma optimize("", on)
+
+TArray<FIKParams> UBaseAnimInstance::UpdateIKs()
+{
+    ACharacter* character = Cast<ACharacter>( this->GetOwningActor() );
+
+    if (!character) 
+    {
+        return TArray<FIKParams>();
+    }
+    
+    TArray<FName> iks;
+    this->IKParams.GetKeys(iks);
+
+    for (FName currentIk : iks) 
+    {
+        bool hitted = false;
+        
+        FIKData ik = this->GetIKData(this->IKParams[currentIk], hitted);
+        this->IKParams[currentIk].StartReferenceLocation = ik.StartReferenceLocation;
+        this->IKParams[currentIk].CurrentLockLocation = ik.Location;
+        this->IKParams[currentIk].Hitted = hitted;
+        this->IKParams[currentIk].Weight = ik.Weight;
+        this->IKParams[currentIk].FinalIKLocation = this->GetRelativeIKLocation(
+            this->IKParams[currentIk].CurrentLockLocation
+        );
+    }
+
+    //this->IsTransitioning = false;
+    if (this->IsTransitioning) 
+    {
+        this->InterpolateIKTransition();
+    }
+
+    return this->GetIKParamsValues();
+
+}
+
+void UBaseAnimInstance::UpdateVelocityStats()
+{
+    FVector currrentVelocity    = this->GetOwningActor()->GetVelocity();
+    FVector horizontalVelocity  = FVector(currrentVelocity.X, currrentVelocity.Y, 0);
+    float currentAcceleration   = horizontalVelocity.Length() - this->LastVelocity.Length();
+
+    this->IsDecelerating        = currentAcceleration < ( this->IdleMoveThreshold * -1 );
+    this->IsAccelerating        = currentAcceleration > this->IdleMoveThreshold;
+
+    if (!this->IsStopping && this->IsDecelerating)
+    {
+        this->IsStopping = this->GetOwningActor()->GetVelocity().Length() < this->IdleMoveThreshold;
+    }
+
+    if (this->IsAccelerating)
+    {
+        this->IsStopping = false;
+    }
+
+    this->LastVelocity = horizontalVelocity;
+}
+
+void UBaseAnimInstance::UpdateReverseMaskStartTraceLocation(FName ikName,FVector newLocation)
+{
+    this->IKParams[ikName].ReverseMaskStartTraceLocation = newLocation;
+}
+
+TArray<FIKParams> UBaseAnimInstance::GetIKParamsValues()
+{
+    TArray<FName> keys;
+    TArray<FIKParams> values;
+    
+    this->IKParams.GetKeys(keys);
+
+    for (FName key : keys) 
+    {
+        values.Add(this->IKParams[key]);
+    }
+    
+    return values;
+}
+
+void UBaseAnimInstance::SetStopping(bool flag)
+{
+    this->IsStopping = flag;
+}
+
+void UBaseAnimInstance::SetInitialIKTransitions(TArray<FTransitIKParams> iksToTransit)
+{
+    for (FTransitIKParams currentIK : iksToTransit)
+    {
+        if (this->IKParams.Contains(currentIK.IKName)) 
+        {
+            currentIK.InitialLocation = this->IKParams[currentIK.IKName].CurrentLockLocation;
+            
+            this->IKTransitionInitialLocation.Add(
+                    currentIK.IKName
+                ,   currentIK
+            );
+        }
+        else
+        {
+            UE_LOG(LogTemp, Log, TEXT("IK settings: %s not found"), *currentIK.IKName.ToString());
+        }
+        
+    }
+}
+
+#pragma optimize("", off)
+void UBaseAnimInstance::InterpolateIKTransition()
+{
+    TArray<FName> iks;
+
+    this->IKTransitionInitialLocation.GetKeys(iks);
+    
+    for (FName ik : iks) 
+    {
+        
+        FTransitIKParams currentTransit = this->IKTransitionInitialLocation[ik];
+        FVector currentInitialLocation = currentTransit.InitialLocation;
+
+        float interpWeight = 0;
+        if (
+            currentTransit.WeightTransitionCurveName.IsValid()
+            && !currentTransit.WeightTransitionCurveName.IsNone()
+        ) {
+            this->GetCurveValue(currentTransit.WeightTransitionCurveName, interpWeight);
+        }
+        else 
+        {
+            interpWeight = currentTransit.TargetWeight;
+        }
+
+        FVector transitingLocation = FMath::Lerp(currentInitialLocation, this->IKParams[ik].StartReferenceLocation, interpWeight);
+        FVector startTrace = FVector(transitingLocation);
+
+        if (this->IKParams[ik].AddRelativeLocationFromReverseMask)
+        {
+            FVector reverseMask = FVector(1) - this->IKParams[ik].StartTraceMask;
+            startTrace += reverseMask * this->IKParams[ik].ReverseMaskStartTraceLocation;
+        }
+
+        FHitResult traceResult;
+        bool hitted = this->GetWorld()->SweepSingleByChannel(
+            traceResult,
+            startTrace,
+            startTrace + (this->IKParams[ik].TraceDirection * this->IKParams[ik].TraceLength),
+            FQuat::Identity,
+            ECollisionChannel::ECC_Visibility,
+            FCollisionShape::MakeSphere(this->IKParams[ik].TraceRadius)
+        );
+
+        
+        if (hitted) 
+        {
+            transitingLocation = traceResult.ImpactPoint + ((this->IKParams[ik].TraceDirection * -1) * this->IKParams[ik].Padding);
+            this->IKParams[ik].FinalIKLocation = this->GetRelativeIKLocation(transitingLocation);
+        }
+        else
+        {
+            this->IKParams[ik].FinalIKLocation = this->GetRelativeIKLocation(transitingLocation);
+        }
+        
+        this->IKParams[ik].CurrentLockLocation = transitingLocation;
+    }
+}
+#pragma optimize("", on)
+
+void UBaseAnimInstance::CleanIKTransitions()
+{
+    this->IKTransitionInitialLocation.Empty();
+}
+
+FVector UBaseAnimInstance::GetRelativeIKLocation(FVector ikLocation)
+{
+    ACharacter* character = Cast<ACharacter>(this->GetOwningActor());
+
+    if (!character)
+    {
+        return FVector();
+    }
+    
+    return FTransform(
+        character->GetMesh()->GetComponentQuat(),
+        character->GetMesh()->GetComponentLocation()
+    ).InverseTransformPosition(ikLocation);
+}
---
+++ Source/G_Lab/Public/Components/AnimInstances/BaseAnimInstance.h
@@ -0,0 +1,206 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BaseAnimInstance.generated.h"
+
+USTRUCT(BlueprintType)
+struct FIKParams 
+{
+	GENERATED_BODY()
+
+public:
+	
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float Weight;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName WeightCurveName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float LockWeight;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName LockWeightCurveName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName RootBone;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName EffectorBone;
+
+	UPROPERTY()
+	FVector StartReferenceLocation;
+
+	UPROPERTY()
+	FVector StartTraceLocation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName StartTraceBoneReference;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool AddRelativeLocationFromReverseMask;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector StartTraceMask{ FVector::Zero() };
+
+	UPROPERTY(BlueprintReadWrite)
+	FVector ReverseMaskStartTraceLocation{ FVector::Zero() };
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector TraceDirection;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float TraceLength;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float Padding;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float TraceRadius;
+
+	UPROPERTY(BlueprintReadOnly)
+	FVector CurrentLockLocation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector FinalIKLocation;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool Hitted;
+
+};
+
+USTRUCT(BlueprintType)
+struct FIKData
+{
+	GENERATED_BODY()
+
+public:
+
+	FIKData() {};
+
+	FIKData(
+			FVector reference
+		,	FVector location
+		,	float weight
+	): 
+		StartReferenceLocation(reference)
+	,	Location(location)
+	,	Weight(weight)
+	{};
+
+	UPROPERTY()
+	FVector StartReferenceLocation;
+
+	UPROPERTY()
+	FVector Location;
+
+	UPROPERTY()
+	float Weight;
+};
+
+USTRUCT(BlueprintType, Blueprintable)
+struct FTransitIKParams 
+{
+	GENERATED_BODY()
+
+public:
+
+	FTransitIKParams() {};
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName IKName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName WeightTransitionCurveName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float TargetWeight;
+
+	UPROPERTY(BlueprintReadOnly)
+	FVector InitialLocation;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class G_LAB_API UBaseAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	
+	/*****
+	* IKs
+	*****/
+	UFUNCTION(BlueprintCallable)
+	FIKData GetIKData(const FIKParams& ikParams, bool& hitted);
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FIKParams> UpdateIKs();
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Settings|IKs")
+	TMap<FName, FIKParams> IKParams;
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateReverseMaskStartTraceLocation(FName ikName, FVector newLocation);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = true)
+	TArray<FIKParams> GetIKParamsValues();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Movement")
+	bool StoppingMovementAnimEnabled;
+
+	UFUNCTION(BlueprintCallable)
+	void SetStopping(bool flag);
+
+	/***************
+	* VELOCITY STATS
+	****************/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Movement")
+	float IdleMoveThreshold;
+	
+	UPROPERTY(BlueprintReadOnly)
+	bool IsStopping;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool IsDecelerating;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool IsAccelerating;
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateVelocityStats();
+
+	UPROPERTY()
+	FVector LastVelocity{ FVector::Zero() };
+
+	/***********
+	* TRANSITION
+	************/
+	UPROPERTY(BlueprintReadWrite)
+	bool IsTransitioning;
+
+	UPROPERTY()
+	TMap<FName, FTransitIKParams> IKTransitionInitialLocation;
+
+	UFUNCTION(BlueprintCallable)
+	virtual void SetInitialIKTransitions(TArray<FTransitIKParams> iksToTransit);
+
+	UFUNCTION(BlueprintCallable)
+	virtual void InterpolateIKTransition();
+
+	UFUNCTION(BlueprintCallable)
+	virtual void CleanIKTransitions();
+	
+	FVector GetRelativeIKLocation(FVector ikLocation);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Movement")
+	bool MovingIdleTransitAnimEnabled;
+
+};
